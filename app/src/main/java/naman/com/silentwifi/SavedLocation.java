f72e8f82e08c89b6821492c5edf109f2869c8981
NamanArora <naman.arora11@gmail.com>
2017-11-05 14:36:26 +0530

    Add a screen to show locations

---
+++ app/src/main/java/naman/com/silentwifi/SavedLocation.java
@@ -0,0 +1,13 @@
+package naman.com.silentwifi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_location);
+    }
+}
