2aefd0dbb9286c726d6132be163f7b2dab3f9f5a
NamanArora <naman.arora11@gmail.com>
2017-11-05 15:16:34 +0530

    Make changes for adapter

--- app/src/main/java/naman/com/silentwifi/SavedLocation.java
+++ app/src/main/java/naman/com/silentwifi/SavedLocation.java
@@ -2,8 +2,23 @@ package naman.com.silentwifi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import naman.com.silentwifi.adapters.LocationAdapter;
 
 public class SavedLocation extends AppCompatActivity {
+
+    ArrayList locations;
+
+    @BindView(R.id.locationRecycler)
+    RecyclerView recycler;
+    LocationAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +28,16 @@ public class SavedLocation extends AppCompatActivity {
     }
 
     private void init() {
+        ButterKnife.bind(this);
+        fetchData();
+        LinearLayoutManager layout = new LinearLayoutManager(this);
+        recycler.setLayoutManager(layout);
+        adapter = new LocationAdapter(locations);
+        recycler.setAdapter(adapter);
+    }
+
+    private void fetchData() {
+
+        adapter.notifyDataSetChanged();
     }
 }
