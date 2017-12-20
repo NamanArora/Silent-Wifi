041186972a973b51f50f17918d6689ff57e402cd
NamanArora <naman.arora11@gmail.com>
2017-12-20 16:19:23 +0530

    Fetch saved locations from db

--- app/src/main/java/naman/com/silentwifi/SavedLocation.java
+++ app/src/main/java/naman/com/silentwifi/SavedLocation.java
@@ -1,5 +1,7 @@
 package naman.com.silentwifi;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,10 +12,11 @@ import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import naman.com.silentwifi.adapters.LocationAdapter;
+import naman.com.silentwifi.models.Location;
 
 public class SavedLocation extends AppCompatActivity {
 
-    ArrayList locations;
+    ArrayList<Location> locations;
 
     @BindView(R.id.locationRecycler)
     RecyclerView recycler;
@@ -31,14 +34,23 @@ public class SavedLocation extends AppCompatActivity {
         ButterKnife.bind(this);
         LinearLayoutManager layout = new LinearLayoutManager(this);
         recycler.setLayoutManager(layout);
-        locations = new ArrayList();
+        locations = new ArrayList<>();
         adapter = new LocationAdapter(locations);
         recycler.setAdapter(adapter);
         fetchData();
     }
 
     private void fetchData() {
-
+        CoordinatesStorage store = new CoordinatesStorage(getApplicationContext());
+        SQLiteDatabase db = store.getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM LOCATION", null);
+        while(c.moveToNext()){
+            String title = c.getString(c.getColumnIndex(CoordinatesStorage.DB.title));
+            float latitude = c.getFloat(c.getColumnIndex(CoordinatesStorage.DB.latitude));
+            float longitude = c.getFloat(c.getColumnIndex(CoordinatesStorage.DB.longitude));
+            Location location = new Location(title,latitude,longitude);
+            locations.add(location);
+        }
         adapter.notifyDataSetChanged();
     }
 }
